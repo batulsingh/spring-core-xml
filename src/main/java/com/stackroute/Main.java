c3f9cdb5a5be4e51daa5fb82bbf35d8bde3bbbe2
bhaiyaatulsingh <46678692+bhaiyaatulsingh@users.noreply.github.com>
2019-10-09 15:45:16 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,34 +12,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.context.support.StaticApplicationContext;
 
 
-public class Main {
-    public static void main(String[] args) {
-        beanFactory();
-        appContext();
-        beanDefinition();
+public class Main
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Movie  movieFirst = context.getBean("movieA", Movie.class);
+        System.out.println(movieFirst.toString());
+        Movie movieSecond = context.getBean("movieB", Movie.class);
+        System.out.println(movieSecond.toString());
+        System.out.println(movieFirst == movieSecond);
     }
-
-    public  static void beanFactory(){
-        System.out.println("BeanFactory");
-        XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie = (Movie) beanFactory.getBean("movie");
-        System.out.println(movie.getActor());
-    }
-
-    private static void beanDefinition() {
-        System.out.println("BeanDefinition");
-        StaticApplicationContext staticApplicationContext = new StaticApplicationContext();
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(staticApplicationContext);
-        reader.loadBeanDefinitions("beans.xml");
-    }
-
-    private static void appContext() {
-        System.out.println("ApplicationContext");
-        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(
-                "beans.xml");
-        Movie movie = (Movie) appContext.getBean("movie");
-        System.out.println(movie.getActor());
-    }
-
-
 }
