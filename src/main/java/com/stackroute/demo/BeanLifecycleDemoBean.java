8e01c9637243a204b457bb0a10f24dceb7f2c812
bhaiyaatulsingh <46678692+bhaiyaatulsingh@users.noreply.github.com>
2019-10-09 16:54:21 +0530

    Update BeanLifecycleDemoBean.java

--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -1,1 +1,37 @@
+package com.stackroute.demo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.DisposableBean;
+
+
+public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Container destroyed.");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initialized bean - Message : " + message);
+    }
+
+    //Custom init-method
+    public void customInit(){
+        System.out.println("In initialization function");
+    }
+
+    //Custome destroy-method
+    public void customDestroy(){
+        System.out.println("In destroy function");
+    }
+}
